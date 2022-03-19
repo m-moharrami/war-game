8d1d4e0db3b670599397d31c62b70ed03ca4130f
Moharram Moharrami <moharram.m98@gmail.com>
2022-03-19 21:47:57 +0330

    initialize the project

---
+++ code.js
@@ -0,0 +1,26 @@
+let deckId;
+
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            deckId = data.deck_id;
+        })
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick);
+
+function drawCards() {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.cards);
+            document.getElementById("cards").innerHTML = `
+                <img src=${data.cards[0].image} />
+                <img src=${data.cards[1].image} />
+            `
+        })
+}
+
+document.getElementById("draw-cards").addEventListener("click", drawCards);
