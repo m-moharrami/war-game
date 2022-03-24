4072e88c72bab92b2e41233bc17775f362cd8963
Moharram Moharrami <moharram.m98@gmail.com>
2022-03-24 23:28:48 +0430

    refactor code: assign DOM elements to their own variables to avoid repeating code

--- code.js
+++ code.js
@@ -1,4 +1,7 @@
 let deckId;
+const cardsContainer = document.getElementById("cards");
+const newDeckButton = document.getElementById("new-deck");
+const drawCardsButton = document.getElementById("draw-cards");
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -9,19 +12,19 @@ function handleClick() {
         })
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick);
+newDeckButton.addEventListener("click", handleClick);
 
 function drawCards() {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            document.getElementById("cards").children[0].innerHTML = `
+            cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
-            document.getElementById("cards").children[1].innerHTML = `
+            cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
         })
 }
 
-document.getElementById("draw-cards").addEventListener("click", drawCards);
+drawCardsButton.addEventListener("click", drawCards);
