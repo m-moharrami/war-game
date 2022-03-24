39ddb11f29a89fd01ecafdaf557e96c59c7357d8
Moharram Moharrami <moharram.m98@gmail.com>
2022-03-24 23:21:56 +0430

    fix card image placeholder

--- code.js
+++ code.js
@@ -15,10 +15,11 @@ function drawCards() {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.cards);
-            document.getElementById("cards").innerHTML = `
-                <img src=${data.cards[0].image} />
-                <img src=${data.cards[1].image} />
+            document.getElementById("cards").children[0].innerHTML = `
+                <img src=${data.cards[0].image} class="card" />
+            `
+            document.getElementById("cards").children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
             `
         })
 }
