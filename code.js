e09685f5a5531220c8ab312d7e1f186349338c50
Moharram Moharrami <moharram.m98@gmail.com>
2022-03-25 17:24:42 +0430

    write function for determining the winner

--- code.js
+++ code.js
@@ -28,3 +28,29 @@ function drawCards() {
 }
 
 drawCardsButton.addEventListener("click", drawCards);
+
+function determineWinner(card1, card2) {
+    const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"];
+    const card1ValueIndex = cardValues.indexOf(card1.value);
+    const card2ValueIndex = cardValues.indexOf(card2.value);
+
+    console.log("card 1: " + card1ValueIndex);
+    console.log("card 2: " + card2ValueIndex);
+
+    if (card1ValueIndex > card2ValueIndex) {
+        console.log("card 1 wins!");
+    } else if (card1ValueIndex < card2ValueIndex) {
+        console.log("card 2 wins!");
+    } else {
+        console.log("it's a tie!");
+    }
+}
+
+const card1Obj = {
+    value: "2"
+};
+const card2Obj = {
+    value: "ACE"
+};
+
+determineWinner(card1Obj, card2Obj);
