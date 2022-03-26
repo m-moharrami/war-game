86c2e93ebc114d74dac2c94bbb027437b04e88d8
Moharram Moharrami <moharram.m98@gmail.com>
2022-03-26 16:31:24 +0430

    keep score for each player and update it after each round

--- code.js
+++ code.js
@@ -1,9 +1,13 @@
 let deckId;
+let computerScore = 0;
+let playerScore = 0;
 const cardsContainer = document.getElementById("cards");
 const newDeckButton = document.getElementById("new-deck");
 const drawCardsButton = document.getElementById("draw-cards");
 const gameInfo = document.getElementById("game-info");
 const remainingText = document.getElementById("remaining");
+const computerScoreContainer = document.getElementById("computer-score");
+const playerScoreContainer = document.getElementById("player-score");
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -44,8 +48,12 @@ function determineWinner(card1, card2) {
     const card2ValueIndex = cardValues.indexOf(card2.value);
 
     if (card1ValueIndex > card2ValueIndex) {
+        computerScore++;
+        computerScoreContainer.textContent = `Computer score: ${computerScore}`;
         return "Computer wins!";
     } else if (card1ValueIndex < card2ValueIndex) {
+        playerScore++;
+        playerScoreContainer.textContent = `My score: ${playerScore}`;
         return "You win!";
     } else { 
         return "WAR!";
